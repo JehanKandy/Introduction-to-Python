da7ad7167c1269f4a0507725ab8db9e09c71afb7
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-10-05 13:28:58 +0530

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,62 @@
+print("Introduction to Python")
+i = int(8);  f = float(8.36)
+print("Integer is : ",i," ***  Float is : ",f)
+
+
+print("You can use # for make a comment")
+#This is a comment
+
+print("_______________________________________________________________")
+print("Conditional Statements in Python")
+
+print("If statement in Python")
+marks = float(75)
+
+
+if marks >= 50:
+    print("PASS")
+
+print("--------------------------")
+print("If else statement in python")
+if marks >= 50:
+    print("PASS")
+else:
+    print("FAIL")
+
+print("--------------------------")
+print("If else if statement in python")
+if marks >= 75:
+    print("A")
+elif marks >= 65:
+    print("B")
+elif marks >= 50:
+    print("C")
+else:
+    print("FAIL")
+
+print("--------------------------")
+print("Nested if statement in python")
+
+n1 = int(50)
+
+if n1 >= 10:
+    if n1 <= 20:
+        print("You Win")
+    else:
+        print("You Loss")
+else:
+    print("You Loss")
+
+print("_______________________________________________________________")
+
+
+
+
+
+
+
+
+
+
+
+
